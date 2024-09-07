e50125c6e59cb1d43509f5168446aeb5544c84a2
Ekkapong <ekkapongn@outlook.com>
2024-09-07 15:02:46 +0700

    refactor : random controller

---
+++ src/Playground.API/Controllers/RandomController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Playground.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class RandomController : ControllerBase
+{
+
+    private readonly ILogger<RandomController> _logger;
+
+    public RandomController(ILogger<RandomController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetRandomNumber")]
+    public int Get()
+    {
+        return new Random().Next(1, 100);
+    }
+}
+
+
