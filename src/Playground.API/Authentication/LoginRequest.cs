5d8ba1c528c170fd16930f4daf2ece6caaf68c22
Ekkapong <ekkapongn@outlook.com>
2024-09-15 21:15:24 +0700

    refactor class

--- src/Playground.API/Authentication/LoginRequest.cs
+++ src/Playground.API/Authentication/LoginRequest.cs
@@ -5,15 +5,4 @@ public class LoginRequest
     public string Username { get; set; }
     public string Password { get; set; }
 }
-public class KeyCloakSigninResponse
-{
-    public string access_token { get; set; }
-    public int expires_in { get; set; }
-    public int refresh_expires_in { get; set; }
-    public string refresh_token { get; set; }
-    public string token_type { get; set; }
-    public int notbeforepolicy { get; set; }
-    public string session_state { get; set; }
-    public string scope { get; set; }
-}
 
