d10e9418bee1226375cea75b33205def0b61b71b
Ekkapong <ekkapongn@outlook.com>
2024-09-15 21:13:02 +0700

    feat : add refresh token keycloak

--- src/Playground.API/Controllers/AuthController.cs
+++ src/Playground.API/Controllers/AuthController.cs
@@ -45,4 +45,27 @@ public class AuthController : ControllerBase
         return Ok(responseJson);
     }
 
+    [HttpPost("refresh")]
+    [Authorize]
+    public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequest request)
+    {
+        using var content = new FormUrlEncodedContent(
+       [
+           new KeyValuePair<string, string>("client_id", _clientId),
+            new KeyValuePair<string, string>("client_secret",_clientSecret),
+            new KeyValuePair<string, string>("grant_type", "refresh_token"),
+            new KeyValuePair<string, string>("refresh_token", request.RefreshToken),
+        ]);
+
+        HttpResponseMessage httpResponseMessage = await _httpClient.PostAsync("token", content);
+
+        httpResponseMessage.EnsureSuccessStatusCode();
+
+        var response = await httpResponseMessage.Content.ReadAsStringAsync();
+        var responseJson = JsonSerializer.Deserialize<KeyCloakSigninResponse>(response);
+
+        // Implement token refresh logic using Keycloak
+        return Ok(responseJson);
+    }
+
 }
